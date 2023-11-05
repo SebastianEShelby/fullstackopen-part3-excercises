26060fe444bc53611b16073f60d1252c6121bb2b
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-05 13:38:02 -0500

    part 3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express();
 app.use(express.json())
+app.use(cors());
 
 morgan.token('request-body', (req, res) => {
   if (req.method !== 'POST') return;
