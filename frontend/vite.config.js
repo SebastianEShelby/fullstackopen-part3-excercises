6eb99e14201ff00c2780a83806b71b41e704f798
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-05 14:28:21 -0500

    part 3.11 phonebook full stack

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    // make sure that the frontend still works locally
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
