58fbe231f90a9cd0dedcbc8191e49f338a193d85
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-07 11:51:48 -0500

    part 3.12: Command-line database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+
 const app = express();
 app.use(express.json())
 app.use(cors());
---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+if (!process.env.MONGO_USERNAME || !process.env.MONGO_PASSWORD) {
+  console.log('set username and password in .env file')
+  process.exit(1)
+}
+
+const url =
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@homebrew.gbcyj2u.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length > 2) {
+
+  const name = process.argv[2]
+  const number = process.argv[3]
+
+  if (!name || !number) {
+    console.log('please pass a valid name and number in commandline arguments')
+    process.exit(2);
+  }
+
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number ?? 'Number Not found'} to phonebook`)
+    mongoose.connection.close()
+  })
+
+} else {
+
+  Person.find({}).then(people => {
+    console.log('phonebook:')
+    people.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+
+}
